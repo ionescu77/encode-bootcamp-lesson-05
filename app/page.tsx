43ac3a5d9ca23f123538fe9453b6d6f3715739d6
Razvan Ionescu <ionescu77@gmail.com>
2024-08-27 10:59:36 +0300

    Lesson5 Exercise2 Step10 Add some page elements

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,20 @@ export default function Home() {
   return (
     <>
       <div className="flex flex-col items-center justify-center min-h-screen">
-        <h1 className="text-4xl font-bold mb-4 text-blue-600">Hello World</h1>
-        <p className="text-xl text-gray-500">This is a test</p>
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-4xl font-bold mb-4 text-blue-600">Hello World</h1>
+          <p className="text-xl text-gray-500">This is a test</p>
+        </div>
+        <div className="flex flex-row items-center justify-center py-4">
+          <input
+            type="text"
+            placeholder="Enter your name"
+            className="bg-gray-200 p-2 rounded-md mr-2"
+          />
+          <button className="bg-blue-600 text-white p-2 rounded-md">
+            Submit
+          </button>
+        </div>
       </div>
     </>
   );
