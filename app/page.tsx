cd24ecdda069fb95f892909e68928076e2ac3838
Razvan Ionescu <ionescu77@gmail.com>
2024-08-27 10:58:30 +0300

    Lesson5 Exercise2 Step9 Add TailwindCSS

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 export default function Home() {
   return (
     <>
-      <h1>Hello World</h1>
-      <p>This is a test</p>
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <h1 className="text-4xl font-bold mb-4 text-blue-600">Hello World</h1>
+        <p className="text-xl text-gray-500">This is a test</p>
+      </div>
     </>
   );
 }
